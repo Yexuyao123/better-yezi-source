8dea60e4b8d0621b4889a8cb1e257c1b4302f1cc
yyyexy <584604539@qq.com>
2021-06-02 14:54:33 +0800

    info download

---
+++ src/components/Info.vue
@@ -0,0 +1,363 @@
+<template>
+  <div class="info">
+    <div class="info-sidebar">
+      <div class="info-avatar section-container">
+        <img src="@/assets/avatar.png" alt="" />
+        <span class="info-l-blue">叶旭瑶</span>
+        <span class="info-m-gray">求职意向：前端工程师</span>
+        <span class="info-m-gray">薪资意向：6-7 k</span>
+      </div>
+      <div class="some-about section-container">
+        <span class="info-l-blue">相关信息</span>
+        <ul>
+          <li class="info-m-gray">
+            <span class="disc-icon">年龄：27</span>
+          </li>
+          <li class="info-m-gray">
+            <span class="disc-icon">地址：厦门</span>
+          </li>
+          <li class="info-m-gray">
+            <span class="disc-icon">电话：188****7086</span>
+          </li>
+          <li class="info-m-gray">
+            <span class="disc-icon">邮箱：584604539@qq.com</span>
+          </li>
+        </ul>
+      </div>
+      <div class="about-diploma section-container">
+        <span class="info-l-blue">教育经历</span>
+        <ul>
+          <li class="info-m-gray">
+            <span class="disc-icon">厦门理工学院 全日制本科</span>
+          </li>
+          <li class="info-m-gray">
+            <span class="disc-icon">2012.09 - 2016.06</span>
+          </li>
+          <li class="info-m-gray">
+            <span class="disc-icon">土木工程（道路与桥梁方向）</span>
+          </li>
+        </ul>
+      </div>
+      <div class="about-work section-container">
+        <span class="info-l-blue">工作经历</span>
+        <div class="work-section">
+          <span class="info-m-gray-bold">2017.03 - 2019.07</span>
+          <span class="info-m-gray-bold"
+            >中国华西工程设计建设有限公司厦门分公司（甲级）</span
+          >
+          <span class="info-m-gray">担任：道路/市政道路设计</span>
+          <span class="info-m-gray">精通AutoCAD，熟练PS</span>
+        </div>
+        <div class="work-section">
+          <span class="info-m-gray-bold">2019.11 - 2020.09 </span>
+          <span class="info-m-gray-bold">厦门市阳光彼岸教育</span>
+          <span class="info-m-gray">担任：高中数学老师</span>
+        </div>
+        <div class="work-section">
+          <span class="info-m-gray-bold">2020.10 - 至今 </span>
+          <span class="info-m-gray long-para blank-before"
+            >由于对行业的兴趣规划转行IT。作为衔接过渡阶段，原本是希望能够在教育机构里边工作边学习前端，由于公司缺教师，带班多且工作任务大，没什么时间学习，于是辞职停下工作自学前端至今。</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="info-main">
+      <div class="about-me section-container">
+        <span class="info-l-blue">关于我</span>
+        <span class="info-m-gray long-para"
+          ><span class="blank-before"
+            >本人毕业于土木工程，从本科阶段至工作阶段和土木行业相处了近十年，由于女生在行业内发展的局限性，加上本人对于IT行业的兴趣，于2020年9月放下工作自学前端技能至今。</span
+          >
+        </span>
+        <span class="info-m-gray long-para"
+          ><span class="blank-before"
+            >本人具备良好的<span class="info-m-blue">团队意识协作精神</span
+            >；</span
+          ></span
+        >
+        <span class="info-m-gray long-para"
+          ><span class="blank-before"
+            >踏实且热爱学习，大学期间多次获得奖学金；</span
+          ></span
+        >
+        <span class="info-m-gray long-para"
+          ><span class="blank-before"
+            >善于分析并解决问题，适应能力强。</span
+          ></span
+        >
+        <span class="info-m-gray long-para"
+          ><span class="blank-before"
+            ><span class="info-m-blue">种一棵树最好是十年前，其次是现在。</span
+            >只要想做，任何时候都不晚。</span
+          ></span
+        >
+      </div>
+      <div class="about-skill section-container">
+        <span class="info-l-blue">个人技能</span>
+        <ul>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >熟悉
+                <span class="info-m-red">HTML5</span> 语义化和各类标签。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >熟悉常用的 CSS，了解
+                <span class="info-m-red">CSS3</span> 各类概念，熟练掌握
+                <span class="info-m-red">Grid、Flex</span>
+                等各类布局，能完成 CSS 动画。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >熟悉原生 JavaScript，熟悉
+                <span class="info-m-red">原型链、作用域、闭包、this</span>
+                等基本概念，熟练掌握数组、对象的增删改查操作，了解ES6的
+                <span class="info-m-red">解构赋值、Symbol、Promise</span>
+                等新增功能。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >了解并能够使用
+                <span class="info-m-red">NPM、Webpack</span> 等工具。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >熟悉 <span class="info-m-red">Vue.js</span> 的各类API，并使用
+                <span class="info-m-red">Vue-Cli</span> 构建项目。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >了解 <span class="info-m-red">Git</span> 操作。</span
+              >
+            </div>
+          </li>
+          <li class="info-m-gray">
+            <div class="disc-icon">
+              <span class="common-content"
+                >使用
+                <span class="info-m-red">ESLint + Prettier</span>
+                约束代码格式及规范。</span
+              >
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="about-project section-container">
+        <span class="info-l-blue">个人项目</span>
+        <span class="info-m-gray long-para"
+          ><span
+            >备注：请尽量使用
+            <span class="info-m-tip">Chrome内核浏览器</span>
+            浏览以下项目，以确保样式兼容性。</span
+          >
+        </span>
+        <div class="project-section">
+          <span class="info-m-blue">个人网站</span>
+          <span class="info-m-gray bold-weight disc-icon">项目介绍：</span>
+          <span class="info-m-gray blank-before"
+            >项目包含个人导航页、打卡计划页（日常打卡、记录待办事项功能）、成果展示页，未来会逐步完善更多功能。</span
+          >
+          <span class="info-m-gray blank-before"
+            >采用了 <span class="info-m-red">Axios</span> 完成 AJAX
+            请求，并配置了
+            <span class="info-m-blue">Cancellation、Interceptors</span>
+            等模块；采用
+            <span class="info-m-red">Echarts</span>
+            提供的
+            <span class="info-m-blue">柱形图、折线图、日历热力图</span>
+            等直观地展示了日常生活打卡完成成果；使用
+            <span class="info-m-red">devProxy</span> 解决 API
+            接口跨域问题。</span
+          >
+          <span class="info-m-gray disc-icon"
+            ><span class="bold-weight">技术栈：</span>
+            <span class="info-m-gray"
+              ><span class="info-m-red"> Vue.js</span> / element-ui / SCSS /
+              Axios / Echarts</span
+            ></span
+          >
+          <span class="info-m-gray disc-icon link-text"
+            ><span class="bold-weight">项目源码：</span>
+            <span class="info-a-blue"
+              ><a
+                href="https://github.com/Yexuyao123/better-yezi-source"
+                target="_blank"
+                >https://github.com/Yexuyao123/better-yezi-source</a
+              ></span
+            ></span
+          >
+          <span class="info-m-gray disc-icon link-text"
+            ><span class="bold-weight">项目预览：</span>
+            <span class="info-a-blue"
+              ><a href="http://yxy.linzhihui.online/" target="_blank"
+                >http://yxy.linzhihui.online/</a
+              ></span
+            ></span
+          >
+        </div>
+        <div class="project-section">
+          <span class="info-m-blue">社区博客</span>
+          <span class="info-m-gray bold-weight disc-icon">项目介绍：</span>
+          <span class="info-m-gray blank-before"
+            >提供：用户登陆注销；撰写发表、修改、删除博客；查看社区用户最新发表的的博客；查看用户主页等功能。
+          </span>
+          <span class="info-m-gray disc-icon"
+            ><span class="bold-weight">技术栈：</span>
+            <span class="info-m-gray"
+              ><span class="info-m-red"> Vue.js</span> / element-ui / SCSS /
+              Axios / <span class="info-m-red">Vuex</span>
+            </span></span
+          >
+          <span class="info-m-gray disc-icon link-text"
+            ><span class="bold-weight">项目源码：</span>
+            <span class="info-a-blue"
+              ><a
+                href="https://github.com/Yexuyao123/2103shareblog-source"
+                target="_blank"
+                >https://github.com/Yexuyao123/2103shareblog-source</a
+              ></span
+            ></span
+          >
+          <span class="info-m-gray disc-icon link-text"
+            ><span class="bold-weight">项目预览：</span>
+            <span class="info-a-blue"
+              ><a href="http://yxy.share.linzhihui.online/" target="_blank"
+                >http://yxy.share.linzhihui.online/</a
+              ></span
+            ></span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../CSS/base.scss";
+.info {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .info-sidebar {
+    width: 30%;
+    border-right: $gray-6 1px solid;
+    padding-right: 10px;
+    .section-container {
+      width: 100%;
+      padding: 12px 3px;
+      border-bottom: $gray-6 1px solid;
+      ul {
+        li {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+    .info-avatar {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      img {
+        width: 100px;
+        border-radius: 10px;
+      }
+      span {
+        padding-top: 4px;
+      }
+    }
+    .some-about {
+    }
+    .about-work {
+      .work-section {
+        padding: 5px 0;
+        display: flex;
+        flex-direction: column;
+        .long-para {
+          text-align: justify;
+        }
+      }
+    }
+  }
+  .info-main {
+    padding-left: 10px;
+    flex: 1;
+    //border: #f8a5c2 1px solid;
+    .section-container {
+      width: 100%;
+      padding: 12px 3px;
+      border-bottom: $gray-6 1px solid;
+      ul {
+        li {
+          .disc-icon {
+            display: flex;
+            &::before {
+              content: "";
+              display: block;
+              width: 5px;
+              height: 5px;
+              border-radius: 50%;
+              background-color: $gray-8;
+              margin-right: 8px;
+              margin-top: 6px;
+            }
+            .common-content {
+              flex: 1;
+              text-align: justify;
+            }
+          }
+        }
+      }
+    }
+    .about-me {
+      display: flex;
+      flex-direction: column;
+      .long-para {
+        //border: #f8a5c2 1px solid;
+      }
+    }
+    .about-project {
+      display: flex;
+      flex-direction: column;
+      .project-section {
+        padding: 5px 0;
+        display: flex;
+        flex-direction: column;
+        text-align: justify;
+        .link-text {
+          span {
+            a {
+              text-decoration: underline;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Information.vue
+++ src/views/Information.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="information">
-    <div class="information-container">
-      <div class="info-sidebar"></div>
-      <div class="info-main"></div>
+    <div class="main flex-center">
+      <div class="information-container">
+        <Info></Info>
+      </div>
+    </div>
+    <div class="information-download">
+      <div class="download-button" @click="downloadPdf">
+        <a
+          href="http://static.linzhihui.online/pdf/%E5%8F%B6%E6%97%AD%E7%91%B6%E7%AE%80%E5%8E%86.pdf
+"
+          target="_blank"
+          download
+          >下载 PDF</a
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Info from "@/components/Info.vue";
+
 export default {
   name: "Information",
-  components: {},
+  components: { Info },
+  methods: {
+    downloadPdf() {},
+  },
 };
 </script>
 
@@ -20,17 +37,43 @@ export default {
   width: 100%;
   height: 100vh;
   padding: 0 150px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  .information-container {
-    width: designFontSize(668.25);
-    height: designFontSize(925);
-    background-color: white;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.4);
-    display: flex;
-    .info-sidebar {
+  display: grid;
+  grid-template: ". main download" 100%/1fr auto 1fr;
+  .main {
+    grid-area: main;
+    .information-container {
+      width: designFontSize(668.25);
+      height: designFontSize(925);
+      background-color: white;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.4);
+      padding: 15px;
+    }
+  }
+  .information-download {
+    grid-area: download;
+    padding-left: 30px;
+    //border: #f8a5c2 1px solid;
+    .download-button {
+      width: 120px;
+      padding: 6px;
+      border-radius: 6px;
+      text-align: center;
+      margin-top: 93vh;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.4);
+      background-color: $gray-1;
+      color: $gray-8;
+      transition: all 1s;
+      font-size: 14px;
+      &:hover {
+        background-color: $color-primary3;
+        color: white;
+        cursor: pointer;
+      }
+      a {
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
     }
   }
 }
